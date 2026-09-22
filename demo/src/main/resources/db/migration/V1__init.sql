611a874dc0e3fa7a6de8ab07bff25a09aa6ccc7d
Laptop <tolikmalovf@gmail.com>
2026-09-22 09:33:04 +0300

    fix/feat: reworked db migrations

--- demo/src/main/resources/db/migration/V1__init.sql
+++ demo/src/main/resources/db/migration/V1__init.sql
@@ -4,16 +4,3 @@ create table if NOT EXISTS categories(
     name VARCHAR(255) NOT NULL
 );
 
-create table if NOT EXISTS products(
-    id BIGINT unsigned auto_increment PRIMARY KEY,
-    category_id BIGINT,
-    sku varchar(40) not null unique,
-    name varchar(200) not null,
-    price DECIMAL(12, 2) not null,
-    stock_quantity INT unsigned not null default 0,
-    is_active BOOLEAN not null default true,
-    constraint chk_product_price check(price > 0),
-    constraint chk_products_name_not_blank check (TRIM(name) <> ''),
-    constraint fk_products_category foreign key (category_id) references categories(category_id) on delete restrict,
-    index idx_products_category (category_id)
-);
---
+++ demo/src/main/resources/db/migration/V2_adding_products.sql
@@ -0,0 +1,15 @@
+alter table categories MODIFY id BIGINT UNSIGNED AUTO_INCREMENT;
+
+create table if NOT EXISTS products(
+    id BIGINT unsigned auto_increment PRIMARY KEY,
+    category_id BIGINT UNSIGNED,
+    sku varchar(40) not null unique,
+    name varchar(200) not null,
+    price DECIMAL(12, 2) not null,
+    stock_quantity INT unsigned not null default 0,
+    is_active BOOLEAN not null default true,
+    constraint chk_product_price check(price > 0),
+    constraint chk_products_name_not_blank check (TRIM(name) <> ''),
+    constraint fk_products_category foreign key (category_id) references categories(category_id) on delete restrict,
+    index idx_products_category (category_id)
+);
