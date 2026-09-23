7983703609ffe17853fb03f3d1129f2f2d46e724
Laptop <tolikmalovf@gmail.com>
2026-09-23 10:00:31 +0300

    refactor/feat: rework on Category&Product DTO's

--- demo/src/main/java/com/pc_hardware_shop/demo/dto/CategoryDTO.java
+++ demo/src/main/java/com/pc_hardware_shop/demo/dto/CategoryDTO.java
@@ -2,20 +2,12 @@ package com.pc_hardware_shop.demo.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 
 public record CategoryDTO(
 
-        @NotNull(message = "Category's name can not be null")
-
         @NotBlank(message = "Category's name can not be blank")
-
-        @Size(max = 255, message = "Category's name can not be longer 255 characters")
-
-        @Size(min = 1, message = "Category's name must contain at least 1 character")
-
+        @Size(min = 1, max = 255, message = "Category's name must be between 1 and 255 characters")
         String name
-
 ) {}
--- demo/src/main/java/com/pc_hardware_shop/demo/dto/ProductDTO.java
+++ demo/src/main/java/com/pc_hardware_shop/demo/dto/ProductDTO.java
@@ -7,29 +7,26 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class ProductDTO {
+public record ProductDTO (
 
     @NotNull(message = "Category ID cannot be null")
-    private Long categoryId;
+    Long categoryId,
 
     @NotBlank(message = "Product's name cannot be blank")
     @Size(min = 1, max = 200, message = "Product's name must be between 1 and 200 characters")
-    private String name;
+    String name,
 
     @NotBlank(message = "Product's SKU cannot be blank")
     @Size(min = 1, max = 40, message = "Product's SKU must be between 1 and 40 characters")
-    private String sku;
+    String sku,
 
     @NotNull(message = "Product's price cannot be null")
     @DecimalMin(value = "0.0", inclusive = false, message = "Product's price must be greater than 0")
-    private BigDecimal price;
+    BigDecimal price,
 
     @NotNull(message = "Product's stock quantity cannot be null")
     @Min(value = 0, message = "Product's stock quantity cannot be negative")
-    private Integer stockQuantity;
+    Integer stockQuantity,
 
-    private boolean isActive = true;
-}
+    boolean isActive
+){}
