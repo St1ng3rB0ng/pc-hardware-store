ddba29c108c130ae296a6b04094fd7e3cd8040da
Laptop <tolikmalovf@gmail.com>
2026-09-23 10:07:38 +0300

    feat: added ProductRepository

---
+++ demo/src/main/java/com/pc_hardware_shop/demo/repository/ProductRepository.java
@@ -0,0 +1,25 @@
+package com.pc_hardware_shop.demo.repository;
+
+import com.pc_hardware_shop.demo.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    boolean existsByName(String name);
+
+    Optional<Product> findByName(String name);
+
+    void deleteByName(String name);
+
+    Optional<Product> findBySku(String sku);
+
+    boolean existsBySku(String sku);
+
+    List<Product> findByCategoryId(Long categoryId);
+
+    List<Product> findByIsActive(boolean isActive);
+}
