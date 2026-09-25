f13e1a0be223fc6996fabddcf379c4e0cae49889
St1ng3rB0ng <tolikmalovf@gmail.com>
2026-09-25 08:36:28 +0300

    feat: added NotFound(404) in GlobalExceptionHandler

--- demo/src/main/java/com/pc_hardware_shop/demo/config/GlobalExceptionHandler.java
+++ demo/src/main/java/com/pc_hardware_shop/demo/config/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.pc_hardware_shop.demo.config;
 
+import com.pc_hardware_shop.demo.exceprion.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -29,5 +30,12 @@ public class GlobalExceptionHandler {
         Map<String, String> error = new HashMap<>();
         error.put("error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNotFoundException(IllegalArgumentException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
