1106bc515542d64bd39a88956ad326058db1282b
St1ng3rB0ng <tolikmalovf@gmail.com>
2026-09-25 08:25:52 +0300

    feat: added AddressRepository

---
+++ demo/src/main/java/com/pc_hardware_shop/demo/repository/AddressRepository.java
@@ -0,0 +1,34 @@
+package com.pc_hardware_shop.demo.repository;
+
+import com.pc_hardware_shop.demo.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    List<Address> findAddressesByCustomerId(Long customerId);
+
+    List<Address> findAddressesByCity(String city);
+
+    List<Address> findAddressesByStreet(String street);
+
+    List<Address> findAddressesByPostalCode(String postalCode);
+
+    void deleteAddressByCustomerId(Long customerId);
+
+    void deleteAddressByCity(String city);
+
+    void deleteAddressByStreet(String street);
+
+    void deleteAddressByPostalCode(String postalCode);
+
+    boolean existsByCustomerId(Long customerId);
+
+    boolean existsByCity(String city);
+
+    boolean existsByStreet(String street);
+
+    boolean existsByPostalCode(String postalCode);
+}
