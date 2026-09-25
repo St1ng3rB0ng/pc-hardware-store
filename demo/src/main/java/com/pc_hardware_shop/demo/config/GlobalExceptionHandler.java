a0ec6f3aaf46484123bf5f815de2b0edd457396a
St1ng3rB0ng <tolikmalovf@gmail.com>
2026-09-25 09:14:21 +0300

    fix: change method argument to correct Exception

--- demo/src/main/java/com/pc_hardware_shop/demo/config/GlobalExceptionHandler.java
+++ demo/src/main/java/com/pc_hardware_shop/demo/config/GlobalExceptionHandler.java
@@ -33,7 +33,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleNotFoundException(IllegalArgumentException ex) {
+    public ResponseEntity<Map<String, String>> handleNotFoundException(NotFoundException ex) {
         Map<String, String> error = new HashMap<>();
         error.put("error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
